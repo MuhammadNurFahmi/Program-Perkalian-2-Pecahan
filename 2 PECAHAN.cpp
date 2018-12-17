3b3fef13bb1918a8569608f505991b2fae7673f8
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-17 10:04:16 +0700

    Add files via upload

---
+++ 2 PECAHAN.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main () 
+{
+	int P1,P2;
+	
+	cout << "Masukkan Nilai P1 : ";
+	cin >> P1;
+	
+	cout << "Masukkan Nilai P2 : ";
+	cin >> P2;
+	
+	cout << "HASIL = " << P1/P2 << "\n";
+	system("PAUSE");
+	return EXIT_SUCCESS;
+	
+}
